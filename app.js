ecb0060fae3c3b301a0a14ac8d2b536d1dc77dac
Sameer Khan <157288172+Sameerkhan313@users.noreply.github.com>
2024-06-15 18:59:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    let displayValue = document.getElementById('display').value;
+    let result;
+    try {
+        result = eval(displayValue);
+    } catch (e) {
+        result = 'Error';
+    }
+    document.getElementById('display').value = result;
+}
